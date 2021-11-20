ec8ae10329d9e1592b5ee1107a8f514da21bc2f4
Jhostin <jhostin.cruz12346@gmail.com>
2021-11-19 19:35:25 -0600

    la masterPageFunciona

---
+++ Programa.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once 'URL/rutas.php';
+
+
+$section = $_GET[ 'Ruta' ] ?? 'home';
+
+if (!isset($routes[ $section ])) {
+   $section = 404;
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title><?php echo $routes[ $section ][ 'title' ]; ?></title>
+	</head>
+	<body>
+	
+      
+		<header class="">
+			<div class="">
+				<nav class="navegacion">
+					<a href="Programa.php?Ruta=Home">Home</a>
+					<a href="Programa.php?Ruta=Empleados">Empleados</a>
+					<a href="Programa.php?Ruta=Producto">Producto</a>
+					<a href="Programa.php?Ruta=Inventario">Inventario</a>
+					<a href="Programa.php?Ruta=Transacion">Transacion</a>
+
+				</nav>
+			</div>
+		
+        </header>
+
+        <?php
+      require_once 'Seccion/' . $section . '.php';
+      ?>
+		<script src="build/js/bundle.min.js"></script>
+	</body>
+</html>
---
+++ URL/rutas.php
@@ -0,0 +1,18 @@
+<?php
+$routes = [
+   'Home' => [
+      'title' => 'HOME',
+   ],
+   'Empleados' => [
+      'title' => 'EMPLEADOS',
+   ],
+   'Producto' => [
+      'title' => 'PRODUCTO',
+   ],
+   'Inventario' => [
+      'title' => 'INVENTARIO',
+   ],
+   'Transacion' => [
+      'title' => 'TRANSACION',
+   ],
+];
